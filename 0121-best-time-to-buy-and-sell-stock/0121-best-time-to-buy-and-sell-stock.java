0c14c130e36a310bacc6690f63160cd0f7750185
kartik Bhadane <kartikbhadane004@gmail.com>
2026-07-26 16:11:34 +0530

    Time: 1 ms (99.94%), Space: 94.6 MB (15.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int buyPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+         int buyPrice = Integer.MAX_VALUE;
+         int maxProfit  = 0;
 
-        for (int i = 0; i < prices.length; i++) {
-            if (buyPrice < prices[i]) {
-                int profit = prices[i] - buyPrice;
+        for(int i = 0; i < prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice; //profit
                 maxProfit = Math.max(maxProfit, profit);
             } else {
                 buyPrice = prices[i];
             }
         }
         return maxProfit;
-    }
+  }
 }
