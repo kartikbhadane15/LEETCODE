c20a5b5a5175852c7305dd2023d77140909f3b44
kartik Bhadane <kartikbhadane004@gmail.com>
2026-09-08 23:37:20 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.68%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int prev2 = 1; // ways(0)
+        int prev1 = 1; // ways(1)
+
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
