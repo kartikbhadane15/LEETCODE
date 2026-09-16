5e03ef1aa63d37fca7cf7f73954c5d177d089104
kartik Bhadane <kartikbhadane004@gmail.com>
2026-09-17 00:38:28 +0530

    Time: 27 ms (59.96%), Space: 48 MB (18.27%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+class Solution {
+    public int maxDistinct(String s) {
+        HashSet<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            set.add(s.charAt(i));
+        }
+
+        return set.size();
+    }
+}
